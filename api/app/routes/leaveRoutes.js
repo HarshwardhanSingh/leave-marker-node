e4de1237388cc2828a74f8684151ccc383323506
HarshwardhanSingh <harsh.rathore14@gmail.com>
2017-09-13 14:25:31 +0530

    add get/post routes for leaves

--- api/app/routes/leaveRoutes.js
+++ api/app/routes/leaveRoutes.js
@@ -1,5 +1,14 @@
 module.exports = (app, db) => {
   app.get('/leaves', (req, res) => {
-    res.send(`Hello world, you have just hit /leaves route ${process.env.NODE_ENV}`);
+    db.query(`SELECT * FROM Leaves;`, (err, result) => {
+      res.send(result)
+    });
+  });
+
+  app.post('/leaves', (req, res) => {
+    db.query(`INSERT INTO Leaves(reason, start, end, type) VALUES ('${req.body.reason}', '${req.body.start}', '${req.body.end}', '${req.body.type}');`, (err, result) => {
+      if (err) res.status(err.statusCode || 500).json(err);
+      res.status(200).json(result);
+    });
   });
 };
